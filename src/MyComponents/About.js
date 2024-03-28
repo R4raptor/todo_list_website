eb63078fcbd48aed3f3cf0f72f0f620de3de2b68
Harsh <122527965+R4raptor@users.noreply.github.com>
2024-03-29 00:01:52 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+    <div>
+      This is an about component!!!
+       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem nostrum inventore doloribus rem, dolorum esse? Sed consequuntur laudantium repudiandae consectetur atque perferendis asperiores voluptatem molestias, officia ea, rem vitae possimus facere beatae nostrum error!</p>
+    </div>
+  )
+}
